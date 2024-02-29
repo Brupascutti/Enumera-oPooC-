6995d2ef68ac2c29d7a3f0787a78067d20d316c5
Bruno Pascutti <brupascutti@gmail.com>
2024-02-29 12:14:00 -0300

    Enumerção

---
+++ enumeração/Program.cs
@@ -0,0 +1,23 @@
+﻿using enumeração.entities;
+using enumeração.entities.Enums;
+namespace enumeração {
+    class Program {
+        static void Main(string[] args) {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            String txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus OS = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(OS);
+            Console.WriteLine(txt);
+
+        }
+    }
+}
